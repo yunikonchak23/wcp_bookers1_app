b6c6bcc78a6a71db4b36a83a9e82d9b66371e262
yunikonchak23 <uetamago.btssuga@gmail.com>
2021-05-06 16:24:05 +0000

    [Finish]bookers1

--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,7 @@ Rails.application.routes.draw do
   get 'books' => 'books#index'
   post 'books' => 'books#create'
   get 'books/:id' => 'books#show', as:'book'
-  get 'books/:id/ edit' => 'books#edit', as:'edit_book'
+  get 'books/:id/edit' => 'books#edit', as:'edit_book'
   patch 'books/:id' => 'books#update', as:'update_book'
   delete 'books/:id' => 'books#destroy'
 
